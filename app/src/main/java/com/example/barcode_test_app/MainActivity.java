273a810903c50840e0b2a92db27ff7c1cad598ab
Prince Solanki <princesolanki4747@gmail.com>
2025-02-21 17:41:30 +0530

    new

--- app/src/main/java/com/example/barcode_test_app/MainActivity.java
+++ app/src/main/java/com/example/barcode_test_app/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.barcode_test_app;
-
-
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.Manifest;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,9 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button scanButton;
+    Button scanButton,storagebtn;
     final int CAMERA_PERMISSION_CODE = 100;
     final int STORAGE_PERMISSION_CODE = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,34 +30,43 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         scanButton = findViewById(R.id.scanbtn);
+        storagebtn = findViewById(R.id.storagebtn);
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 checkDevicePermissions(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
-                Toast.makeText(MainActivity.this, "Hello This is Barcode Scanner", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "btn clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        storagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkDevicePermissions(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+                checkDevicePermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+                Toast.makeText(MainActivity.this, "btn clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     public void checkDevicePermissions(String permission, int requestCode) {
         // Check for permissions
-        if (ContextCompat.checkSelfPermission(this,permission) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this, new String[] {permission}, requestCode);
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==CAMERA_PERMISSION_CODE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
-            } else if (requestCode == STORAGE_PERMISSION_CODE) {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-
             }
         }
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
